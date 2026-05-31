a330dc76c757b8a69c1b7dc558415aeea73cd937
Humberto López <humbertolpzc.work@gmail.com>
2026-05-30 17:57:05 -0700

    let, cons

---
+++ 00-Reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName = 'Humberto';
+const lastName = 'López';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterH = lastName.includes('Ló');
+
+console.log({ containsLetterH, diceNumber });
